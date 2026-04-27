a8dd71e589c073cc3fb4852e2816ccaac2502bb5
jakirhosain45 <mdjakirhosain853@gmail.com>
2026-04-27 21:14:57 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+
+const loginbtn =document.getElementById('loginbtn')
+
+loginbtn.addEventListener('click',(e)=>{
+    e.preventDefault()
+    const useername =document.getElementById('username').value
+    const password =document.getElementById('password').value
+    if(!useername || !password){
+        alert('please provide username and password')
+    }
+    else if(useername === "admin" && password === "admin123"){
+        
+    }
+    else{
+        alert('Invalid  username or password')
+    }
+    
+})
